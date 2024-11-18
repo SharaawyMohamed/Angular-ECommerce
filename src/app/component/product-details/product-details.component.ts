92f128f2a6eb772c6c7e6e0dd8d8260587e93437
SharaawyMohamed <sharawem585@gmail.com>
2024-11-19 00:57:35 +0200

    fix errors in product details

--- src/app/component/product-details/product-details.component.ts
+++ src/app/component/product-details/product-details.component.ts
@@ -4,37 +4,33 @@ import { ProductService } from '../../Services/product.service';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DiscountPipe } from "../../pipes/discount.pipe";
-import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [CommonModule, DiscountPipe],
+  imports: [CommonModule, DiscountPipe,RouterModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent implements OnInit {
-// get id from router link:
-ProductDetails?:IProduct;
+export class ProductDetailsComponent {
+ProductDetails!:IProduct;
 Id:number;
 
 constructor(
   private _productService:ProductService,
-  productService:ProductService,
   private activeRoute:ActivatedRoute,
   private router:Router
 )
 {
-this._productService=productService;
-this.Id= Number(this.activeRoute.snapshot.paramMap.get('Id'));
+this.Id= Number(this.activeRoute.snapshot.paramMap.get('Id'))||0;
+this.getProductById(this.Id);
+console.log(this.Id);
+if(!this.Id){
+  this.router.navigate(['**']);
 }
-  ngOnInit(): void {
-    this.getProductById(this.Id)
-    if(this.ProductDetails==null){
-      this.router.navigate(['**']);
-    }
 
-  }
+}
+
   getProductById(Id:number):void{
   this._productService.getProductById(Id).subscribe({
     next:(data:IProduct)=>{
